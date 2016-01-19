95558f3a5de897aab042fbea37a534ecde523d62
Jackson Cafazzo and Ryan Brown <doctorjuno@gmail.com>
2016-01-19 11:50:29 -0800

    Setup initial files and first spec (equilateral)

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+debugger
+var triangleSides = function(sideA, sideB, sideC) {
+  return (sideA === sideB && sideB === sideC);
+};
+
+//
+// $(document).ready(function() {
+//   $("form#triangleSides").submit(function(event) {
+//     var triangle = ([sideA, sideB, sideC])
+//     var sideA = parseInt($("input#sideA").val());
+//     var sideB = parseInt($("input#sideB").val());
+//     var sideC = parseInt($("input#sideC").val());
+//     event.preventDefault();
+//   });
+// });
---
+++ spec/specs.js
@@ -0,0 +1,5 @@
+describe('triangleSides', function() {
+  it("is Equilateral", function() {
+    expect(triangleSides(3, 3, 3)).to.equal(true);
+  });
+});
