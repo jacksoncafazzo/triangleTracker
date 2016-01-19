629065db87b0cfbc78ab089c2da20d739ffdf000
Jackson Cafazzo and Ryan Brown <browneryan87@gmail.com>
2016-01-19 14:16:19 -0800

    Refactor into separate functions in scripts.js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,25 @@
-debugger
-  var triangleSides = function(sideA, sideB, sideC) {
-  if (sideA <= (sideB + sideC) && sideB <= (sideC + sideA) && sideC <=(sideA +sideB)) {
-    if (sideA === sideB && sideB===sideC) {
-      return "equilateral";
-    } else if (sideA===sideB || sideA===sideC || sideC===sideB) {
-      return "isosceles";
-    } else {
-      return "scalene";
-    }
+var triangleSides = function(sideA, sideB, sideC) {
+  if (isTriangle(sideA, sideB, sideC)) {
+    return triangleType(sideA, sideB, sideC)
   } else {
     return "These sides do not make a triangle."
   }
 };
+
+var isTriangle = function(sideA, sideB, sideC){
+
+  return sideA <= (sideB + sideC) && sideB <= (sideC + sideA) && sideC <=(sideA +sideB)
+}
+
+var triangleType = function(sideA, sideB, sideC){
+  if (sideA === sideB && sideB===sideC) {
+    return "equilateral triangle";
+  } else if (sideA===sideB || sideA===sideC || sideC===sideB) {
+    return "isosceles triangle";
+  } else {
+    return "scalene triangle";
+  }
+}
 
 $(document).ready(function() {
   $("form#triangleSides").submit(function(event) {
@@ -24,7 +32,8 @@ $(document).ready(function() {
     sideB = parseInt($("input#sideB").val());
     sideC = parseInt($("input#sideC").val());
 
-    $(".result").append(result);
+    $(".result").text(result);
+    $("#result").show();
 
     event.preventDefault();
   });
--- spec/specs.js
+++ spec/specs.js
@@ -1,14 +1,30 @@
-describe('triangleSides', function() {
+describe('isTriangle', function() {
+  it("is triangle", function() {
+    expect(isTriangle(4, 1, 2)).to.equal(false);
+ });
+
+ it("is triangle", function() {
+   expect(isTriangle(2, 2, 2)).to.equal(true);
+});
+});
+
+describe('triangleType', function(){
   it("is Equilateral", function() {
-    expect(triangleSides(3, 3, 3)).to.equal('equilateral');
+    expect(triangleType(3, 3, 3)).to.equal('equilateral');
   });
   it("is Isosceles", function() {
-    expect(triangleSides(3, 2, 3)).to.equal('isosceles');
+    expect(triangleType(3, 2, 3)).to.equal('isosceles');
   });
+  it("is Scalene", function() {
+    expect(triangleType(1, 2, 3)).to.equal('scalene');
+  });
+});
+
+describe('triangleSides', function() {
   it("is Scalene", function() {
     expect(triangleSides(1, 2, 3)).to.equal('scalene');
   });
   it("is not triangle", function() {
-    expect(triangleSides(4, 1, 2)).to.equal('These sides do not make a triangle.');
+    expect(triangleSides(4, 1, 2)).to.equal("These sides do not make a triangle.");
   });
 });
